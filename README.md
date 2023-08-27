831a398984a196871e91ac665ea9385c9c629ac8
Ajmalniz <128520849+Ajmalniz@users.noreply.github.com>
2023-08-27 13:04:46 +0500

    static rendring

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+#fetch is used to get data fro API 
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
--- app/static/page.tsx
+++ app/static/page.tsx
@@ -1,7 +1,27 @@
 import React from 'react'
-
-export default function StaticPage() {
+type Book = {
+id: number;
+name : string;
+type: string;
+available : boolean
+};
+async function getBooks() {
+const response= await fetch("https://simple-books-api.glitch.me/books");
+  const data =response.json();
+  return data;
+}
+export default async function StaticPage() {
+const books =  await getBooks();
   return (
-    <div>Static Page</div>
+    <div>
+      <h1 className="text-xl text-blue-700">Satatic Page</h1>
+    {books.map((book:Book) => 
+      <ul key={book.id}>
+        <li>{book.name} -{book.type}
+        </li>
+
+      </ul>
+     )}
+    </div>
   )
 }
